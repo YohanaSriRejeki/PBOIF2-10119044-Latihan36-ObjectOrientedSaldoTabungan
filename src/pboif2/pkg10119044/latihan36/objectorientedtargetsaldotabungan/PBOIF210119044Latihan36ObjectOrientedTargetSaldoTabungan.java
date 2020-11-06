7f60b7c88e594f5e344c972c7918410d1068bf0e
ASUS <ASUS@ASUS-X541U>
2020-11-06 20:01:54 +0700

    Program ini bertujuan menampilkan program saldo tabungan berbasis object oriented

---
+++ src/pboif2/pkg10119044/latihan36/objectorientedtargetsaldotabungan/PBOIF210119044Latihan36ObjectOrientedTargetSaldoTabungan.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119044.latihan36.objectorientedtargetsaldotabungan;
+
+/**
+ *
+ * @author 
+ * NAMA     : Yohana Sri Rejeki
+ * KELAS    : IF2
+ * NIM      : 10119044
+ * Deskripsi Program : Program ini bertujuan menampilkan program saldo Tabungan
+ * yang berbasis object oriented 
+ */
+public class PBOIF210119044Latihan36ObjectOrientedTargetSaldoTabungan {
+    public static void main(String[] args) {
+        SaldoTabungan saltab = new SaldoTabungan();
+        saltab.saldoAkhir();
+    }
+}
